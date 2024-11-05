c428d568ad7c69674f5644b6f043a6d7dc77b0ec
FermionicChaos <ajossart3@gmail.com>
2024-11-05 14:32:46 -0600

    fixed glslang ambiguity with vulkan sdk

--- src/core/gcl/glslang_util.h
+++ src/core/gcl/glslang_util.h
@@ -6,27 +6,27 @@
 
 #include <geodesy/core/gcl/config.h>
 
-//#include <glslang/Include/arrays.h>
-#include <glslang/Include/BaseTypes.h>
-#include <glslang/Include/Common.h>
-#include <glslang/Include/ConstantUnion.h>
-#include <glslang/Include/intermediate.h>
-#include <glslang/Include/PoolAlloc.h>
-#include <glslang/Include/ResourceLimits.h>
-#include <glslang/Include/SpirvIntrinsics.h>
-#include <glslang/Include/Types.h>
+//#include <glslang-src/glslang/Include/arrays.h>
+#include <glslang-src/glslang/Include/BaseTypes.h>
+#include <glslang-src/glslang/Include/Common.h>
+#include <glslang-src/glslang/Include/ConstantUnion.h>
+#include <glslang-src/glslang/Include/intermediate.h>
+#include <glslang-src/glslang/Include/PoolAlloc.h>
+#include <glslang-src/glslang/Include/ResourceLimits.h>
+#include <glslang-src/glslang/Include/SpirvIntrinsics.h>
+#include <glslang-src/glslang/Include/Types.h>
 
-#include <glslang/MachineIndependent/localintermediate.h>
+#include <glslang-src/glslang/MachineIndependent/localintermediate.h>
 
-#include <glslang/Public/ShaderLang.h>
+#include <glslang-src/glslang/Public/ShaderLang.h>
 
 // Converts shader source into SPIRV.
-//#include <glslang/SPIRV/SpvTools.h>
-//#include <glslang/SPIRV/Logger.h>
-#include <glslang/SPIRV/GlslangToSpv.h>
-//#include <glslang/SPIRV/spirv.hpp>
-//#include <glslang/SPIRV/spvIR.h>
-//#include <glslang/SPIRV/SPVRemapper.h>
+//#include <glslang-src/SPIRV/SPIRV/SpvTools.h>
+//#include <glslang-src/SPIRV/SPIRV/Logger.h>
+#include <glslang-src/SPIRV/GlslangToSpv.h>
+//#include <glslang-src/SPIRV/SPIRV/spirv.hpp>
+//#include <glslang-src/SPIRV/SPIRV/spvIR.h>
+//#include <glslang-src/SPIRV/SPIRV/SPVRemapper.h>
 
 // Included for compiling
 #include "ResourceLimits.h"
