75d639e4b30c1b74fb431f535995837dacd327e5
FermionicChaos <ajossart3@gmail.com>
2024-12-16 19:06:57 -0600

    disabled animated objects.

--- src/bltn/stg/scene3d.cpp
+++ src/bltn/stg/scene3d.cpp
@@ -35,16 +35,16 @@ namespace geodesy::bltn::stg {
 		this->create_object<ecs::object>("Box", 				SpawnObjects[0], 	math::vec<float, 3>(0.0f, 0.0f, 0.0f), 		math::vec<float, 2>(0.0f, 0.0f), 	math::vec<float, 3>(5.0f, 5.0f, 5.0f));
 		this->create_object<ecs::object>("BoxTextured", 		SpawnObjects[1], 	math::vec<float, 3>(10.0f, 0.0f, 0.0f), 	math::vec<float, 2>(0.0f, 0.0f), 	math::vec<float, 3>(5.0f, 5.0f, 5.0f));
 		this->create_object<ecs::object>("Lantern", 			SpawnObjects[2], 	math::vec<float, 3>(20.0f, 0.0f, 0.0f), 	math::vec<float, 2>(-90.0f, 0.0f), 	math::vec<float, 3>(1.0f, 1.0f, 1.0f));
-		this->create_object<ecs::object>("CesiumMilkTruck", 	SpawnObjects[3], 	math::vec<float, 3>(0.0f, 30.0f, 0.0f), 	math::vec<float, 2>(-90.0f, 0.0f), 	math::vec<float, 3>(5.0f, 5.0f, 5.0f));
-		this->create_object<ecs::object>("Pigwithanimation", 	SpawnObjects[4], 	math::vec<float, 3>(-40.0f, 30.0f, 0.0f), 	math::vec<float, 2>(-90.0f, 0.0f), 	math::vec<float, 3>(5.0f, 5.0f, 5.0f));
-		this->create_object<ecs::object>("BrainStem", 			SpawnObjects[5], 	math::vec<float, 3>(40.0f, 30.0f, 0.0f), 	math::vec<float, 2>(0.0f, 0.0f), 	math::vec<float, 3>(10.0f, 10.0f, 10.0f));
-		this->create_object<ecs::object>("Cesium Man", 			SpawnObjects[6], 	math::vec<float, 3>(-20.0f, 0.0f, 0.0f), 	math::vec<float, 2>(-90.0f, 0.0f), 	math::vec<float, 3>(10.0f, 10.0f, 10.0f));
 		this->create_object<ecs::object>("Sponza", 				SpawnObjects[7], 	math::vec<float, 3>(0.0f, 0.0f, 0.0f), 		math::vec<float, 2>(-90.0f, 90.0f), 	math::vec<float, 3>(75.0f, 75.0f, 75.0f));
+		// this->create_object<ecs::object>("CesiumMilkTruck", 	SpawnObjects[3], 	math::vec<float, 3>(0.0f, 30.0f, 0.0f), 	math::vec<float, 2>(-90.0f, 0.0f), 	math::vec<float, 3>(5.0f, 5.0f, 5.0f));
+		// this->create_object<ecs::object>("Pigwithanimation", 	SpawnObjects[4], 	math::vec<float, 3>(-40.0f, 30.0f, 0.0f), 	math::vec<float, 2>(-90.0f, 0.0f), 	math::vec<float, 3>(5.0f, 5.0f, 5.0f));
+		// this->create_object<ecs::object>("BrainStem", 			SpawnObjects[5], 	math::vec<float, 3>(40.0f, 30.0f, 0.0f), 	math::vec<float, 2>(0.0f, 0.0f), 	math::vec<float, 3>(10.0f, 10.0f, 10.0f));
+		// this->create_object<ecs::object>("Cesium Man", 			SpawnObjects[6], 	math::vec<float, 3>(-20.0f, 0.0f, 0.0f), 	math::vec<float, 2>(-90.0f, 0.0f), 	math::vec<float, 3>(10.0f, 10.0f, 10.0f));
 
-		this->Object[4]->AnimationWeights = { 1.0f, 0.0f };
-		this->Object[5]->AnimationWeights = { 1.0f, 0.0f };
-		this->Object[6]->AnimationWeights = { 1.0f, 0.0f };
-		this->Object[7]->AnimationWeights = { 1.0f, 0.0f };
+		// this->Object[5]->AnimationWeights = { 1.0f, 0.0f };
+		// this->Object[6]->AnimationWeights = { 1.0f, 0.0f };
+		// this->Object[7]->AnimationWeights = { 1.0f, 0.0f };
+		// this->Object[8]->AnimationWeights = { 1.0f, 0.0f };
 	}
 
 }
